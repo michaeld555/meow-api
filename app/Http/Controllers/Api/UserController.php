ebbb35f1fe54b41a0160c4c39eab041eac898cb4
Michael Douglas <usermich999@gmail.com>
2022-09-16 23:59:58 -0300

    #1.0

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -67,35 +67,23 @@ class UserController extends BaseController
      */
     public function edit(Request $request, $id)
     {
-        /* try{
-            $user = User::findOrFail($id);
-            $request->validate([
-                'name' => 'required|max:255',
-                'email' => 'required|email|unique:users,email,'.$user->id_user.',id_user'
-            ]);
-            $user->name = $request->name;
-            $user->email = $request->email;
-            $user->save();
-            $this->createRecentActivity("Editou seu cadastro", $user);
-            return $this->sendResponse('[]','Usuário alterado com sucesso');
-        } catch (ModelNotFoundException $e) {
-            return $this->sendError('Usuário não encontrado', 404);
-        } */
-         //if ($request->hasFile('url_image')) {
+            try {
+                $user = User::findOrFail($id);
+                $name = time()."meowProfileImage$id.png";
+                $filePath = "users/$id/profile/$name";
+                $success = Storage::disk('s3')->put($filePath, file_get_contents("data:image/png;base64,$request->url_image"), 'public');
+                if ($success) {
+                    $url_image  = Storage::disk('s3')->url($filePath);
+                    $user->photo_url = $url_image;
+                    $user->save();
+                    return $this->sendResponse($url_image);
+                } else {
+                    return $this->sendError('Erro ao Enviar Imagem', 400);
+                }
 
-            //$file = $request->file('url_image');
-            $name = "teste1234";//time() . $file->hashName();
-            $filePath = "users/$id/profile/$name";
-
-            $success = Storage::disk('s3')->put($filePath, file_get_contents("data:image/png;base64,$request->url_image"), 'public');
-            if ($success) {
-                $url_image  = Storage::disk('s3')->url($filePath);
-                return $url_image;
+            } catch(ModelNotFoundException){
+                return $this->sendError('Usuário não encontrado', 400);
             }
-        //} else {
-        //    $url_image = '';
-        //    return $url_image;
-        //}
     }
 
     /**
